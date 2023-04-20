896ee94a0792bc157ef8cf2f10ee3238ef798bd6
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-20 14:50:30 +0530

    AddTransactions modified

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,27 +1,23 @@
-import React, { useContext } from 'react';
-import { GlobalContext } from '../context/GlobalState';
-import ExpenseTransactions from './ExpenseTransactions';
-
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import ExpenseTransactions from "./ExpenseTransactions";
 
 const ExpenseList = () => {
+  const { expenseTransactions } = useContext(GlobalContext);
 
-    const { expenseTransactions } = useContext(GlobalContext);
-
-
-    return (
-        <div className='transactions transactions-expense'>
-          <h2>Transaction History</h2>
-          <ul className='transaction-list'>
-            <li className='transaction'>
-                <span className='transaction-text'>Rent</span>
-                <span className='transaction-amount'>$500</span>
-                <button className='delete-btn'>
-                    <i className='fas fa-trash'></i>
-                </button>
-            </li>
-          </ul>
-        </div>
-      )
-}
+  return (
+    <div className="transactions transactions-expense">
+      <h2>Transaction History</h2>
+      <ul className="transaction-list">
+        {expenseTransactions.map((expenseTransaction) => (
+          <ExpenseTransactions
+            key={expenseTransaction.id}
+            expenseTransaction={expenseTransaction}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ExpenseList;
