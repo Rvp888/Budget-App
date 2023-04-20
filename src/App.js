845d69ac633b532855fd230db290a6a434a1b648
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-20 12:51:39 +0530

    IncomeList component created

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import Header from './components/Header';
 import Balance from './components/Balance';
 import AddTransaction from './components/AddTransaction';
-
+import IncomeList from './components/IncomeList';
 
 const App = () => {
   return (
@@ -13,6 +13,7 @@ const App = () => {
             <Header />
             <Balance />
             <AddTransaction />
+            <IncomeList />
         </div>
     </div>
   )
